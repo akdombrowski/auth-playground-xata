87846391e7bb0009962080e7d9861c989d63c773
Anthony Dombrowski <akdflyinvee73@gmail.com>
2023-04-29 12:32:52 -0500

    xata fetch data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,67 +1,57 @@
-import type { InferGetServerSidePropsType } from 'next'
-import Image from 'next/image'
-import { getXataClient } from '../utils/xata.codegen'
-import xatafly from '../public/xatafly.gif'
+import type { InferGetServerSidePropsType } from "next";
+import Image from "next/image";
+import { getXataClient } from "../utils/xata.codegen";
+import xatafly from "../public/xatafly.gif";
 
 const pushDummyData = async () => {
-  const response = await fetch('/api/write-links-to-xata')
+  const response = await fetch("/api/write-links-to-xata");
 
   if (response.ok) {
-    window?.location.reload()
+    window?.location.reload();
   }
-}
+};
 
 const removeDummyItem = async (id: string) => {
-  const { status } = await fetch('/api/clean-xata', {
-    method: 'POST',
+  const { status } = await fetch("/api/clean-xata", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({ id }),
-  })
+  });
 
   if (status === 200) {
-    window?.location.reload()
+    window?.location.reload();
   }
-}
+};
 
 export default function IndexPage({
-  links,
+  users,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <main>
       <header>
-        <Image src={xatafly} alt={'Xata'} priority />
+        <Image
+          src={xatafly}
+          alt={"Xata"}
+          priority
+        />
         <h1>
           Next.js with<span aria-hidden>&#8209;</span>xata
         </h1>
       </header>
       <article>
-        {links.length ? (
+        {users.length ? (
           <ul>
-            {links.map(({ id, title, url, description }) => (
-              <li key={url}>
-                <a href={url} rel="noopener noreferrer" target="_blank">
-                  {title}
-                </a>
-                <p>{description}</p>
-
-                <button
-                  type="button"
-                  onClick={() => {
-                    removeDummyItem(id)
-                  }}
-                >
-                  <span role="img" aria-label="delete item">
-                    🗑
-                  </span>
-                </button>
+            {users.map(({ id, credPubKey }) => (
+              <li key={id}>
+                <p>{credPubKey}</p>
               </li>
             ))}
           </ul>
         ) : (
           <section>
-            <h2>No records found.</h2>
+            <h2>No users found.</h2>
             <strong>
               Create a `nextjs_with_xata_example` and push some useful links to
               see them here.
@@ -69,9 +59,8 @@ export default function IndexPage({
             <button
               type="button"
               onClick={() => {
-                pushDummyData()
-              }}
-            >
+                pushDummyData();
+              }}>
               Push records to Xata
             </button>
           </section>
@@ -79,22 +68,25 @@ export default function IndexPage({
       </article>
       <footer>
         <span>
-          Made by{' '}
-          <a href="https://xata.io" rel="noopener noreferrer" target="_blank">
+          Made by{" "}
+          <a
+            href="https://xata.io"
+            rel="noopener noreferrer"
+            target="_blank">
             <object data="/xatafly.svg" />
           </a>
         </span>
       </footer>
     </main>
-  )
+  );
 }
 
 export const getServerSideProps = async () => {
-  const xata = await getXataClient()
-  const links = await xata.db.nextjs_with_xata_example.getAll()
+  const xata = await getXataClient();
+  const users = await xata.db.users.getAll();
   return {
     props: {
-      links,
+      users,
     },
-  }
-}
+  };
+};
